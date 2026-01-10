e9db34f0e9cb2d0b06d839f565c79053f0bc48c3
FitoPero <fito.pero@gmail.com>
2026-01-10 00:40:51 -0300

    test change

--- Vue_app/src/Pages/Welcome.vue
+++ Vue_app/src/Pages/Welcome.vue
@@ -13,7 +13,7 @@ import LayoutMain from '@/Layouts/LayoutMain.vue'
         <section class="min-h-screen bg-gray-100 dark:bg-gray-900 p-5">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center">
                 <div tabindex="0"
-                    class="col-span-full w-full max-w-4xl min-h-96 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex items-center justify-center text-center motion-safe:hover:scale-[1.01] transition-all duration-300 focus:outline focus:outline-2 focus:outline-red-500"
+                    class="col-span-full w-full max-w-4xl min-h-96 p-6 bg-white dark:bg-gray-800/50 from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex items-center justify-center text-center motion-safe:hover:scale-[1.01] transition-all duration-300 focus:outline-red-500"
                 >
                     <p class="text-lg text-gray-800 dark:text-gray-100">
                         Welcome to your Vue.js application!
