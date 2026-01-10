7352ee9f3ce8241824de142959cc633e38e24a51
FitoPero <fito.pero@gmail.com>
2026-01-09 23:09:47 -0300

    first commit

---
+++ Laravel_app/vite.config.js
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
+
+// import { defineConfig, loadEnv } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, process.cwd(), '');
+
+//     return {
+//         server: {
+//             host: '0.0.0.0',
+//             port: env.VITE_HMR_PORT,
+//             strictPort: true,
+//             watch: {
+//                 usePolling:'true',
+//             },
+//             hmr: {
+//                 host: env.VITE_HMR_HOST,
+//                 port: env.VITE_HMR_PORT,
+//                 protocol: env.VITE_HMR_PROTOCOL,
+//             }
+//         },
+//         plugins: [
+//             laravel({
+//                 input: ['resources/css/app.css', 'resources/js/app.js'],
+//                 refresh: true,
+//             }),
+//         ],
+//     };
+// });
